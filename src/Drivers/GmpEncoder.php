adfd1953728542313703a115d0d2ccab7573abf8
Siro Díaz Palazón <sirodiaz93@gmail.com>
2018-05-31 23:56:23 +0200

    Added GmpEncoder driver and same tests for it

--- src/Drivers/GmpEncoder.php
+++ src/Drivers/GmpEncoder.php
@@ -3,19 +3,20 @@
 namespace Base62\Drivers;
 
 use Base62\Drivers\BaseEncoder;
+use InvalidArgumentException;
 
-class GmpEncoder implements BaseEncoder
+class GmpEncoder extends BaseEncoder
 {
     public function encode($number)
     {
         if (!preg_match("/^(\d)+$/", (string) $number)) {
-            return false;
+            throw new InvalidArgumentException('The value can only accept numbers');
         }
-
-        if ($number == 0) {
+        if ($number === 0) {
             return '0';
         }
         
+        /*
         $gmpNumber = gmp_init($number);
         $encodedNumber = '';
         while (gmp_cmp($gmpNumber, '0') === 1) {
@@ -23,12 +24,21 @@ class GmpEncoder implements BaseEncoder
             $encodedNumber = $this->$chars[(int)((string)$remainder)] . $encodedNumber;
             $number = $number->dividedBy(62, RoundingMode::DOWN);
         }
+        */
 
-        return $encodedNumber;
+        $base62 = gmp_strval(gmp_init($number, 10), 62);
+
+        return $base62;
     }
 
     public function decode($data)
     {
+        if (!is_string($data)) {
+            throw new InvalidArgumentException('Must be a base 62 valid string');
+        }
+
+        $hex = gmp_strval(gmp_init($data, 62), 10);
         
+        return $hex;
     }
 }
