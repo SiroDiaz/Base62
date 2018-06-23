7b3b5d18201d690ab39ba4f8f0791cfd6fc6ffff
Siro Diaz <sirodiaz93@gmail.com>
2018-06-23 11:03:54 +0200

    Removed old code in service provider

--- src/Base62ServiceProvider.php
+++ src/Base62ServiceProvider.php
@@ -60,10 +60,6 @@ class Base62ServiceProvider extends ServiceProvider
             $this->publishes($paths, $group);
         }
         */
-
-        $this->publishes([
-            __DIR__.'/../config/base62.php' => config_path('base62.php')
-        ], 'base62/base62');
     }
 
     /**
@@ -78,6 +74,10 @@ class Base62ServiceProvider extends ServiceProvider
         } elseif ($this->app instanceof LumenApplication) {
             $this->app->configure('base62');
         }
+
+        $this->publishes([
+            __DIR__.'/../config/base62.php' => config_path('base62.php')
+        ], 'base62/base62');
         
         $this->mergeConfigFrom(
             dirname(__DIR__).'/config/base62.php', 'base62'
